5c2acfb3d20afdadabcfc3f3e8c4d84fee5d94b7
Brandon Couto <22092@columbushs.com>
2019-12-09 08:43:36 -0500

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,21 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
+  // Viking
+  class Viking{}
 
-// Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
-
-// War
-class War {}
+  // Saxon
+  class Saxon {}
+  
+  // War
+  class War {}
